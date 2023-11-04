28850b773740f0366deaca42a0b348ee2b823537
t-NumericalCatalyst <tszyrowski@gmail.com>
2023-11-04 15:53:54 +0000

    mian: Adding sql for remote
    Waht:
    The app was deployed
    The sql dump and its modified version added
    Why:
    To see the app on the wire

---
+++ attendance/attende_modified.sql
@@ -0,0 +1,80 @@
+-- MariaDB dump 10.19 Distrib 10.4.24-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost Database: attendance
+-- ------------------------------------------------------
+-- Server version 10.4.24-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `specialties`
+--
+
+CREATE TABLE IF NOT EXISTS `specialties` (
+  `specialty_id` int(11) NOT NULL AUTO_INCREMENT,
+  `specialty_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`specialty_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `specialties`
+--
+
+INSERT INTO `specialties` VALUES
+(1,'Database Admin'),
+(4,'Software Dev'),
+(6,'DevOps'),
+(7,'Security'),
+(8,'Data Analyst'),
+(9,'Other');
+
+--
+-- Table structure for table `attende`
+--
+
+CREATE TABLE IF NOT EXISTS `attende` (
+  `attendee_id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `dateofbirth` date NOT NULL,
+  `emailaddress` varchar(100) NOT NULL,
+  `contactnumber` varchar(15) NOT NULL,
+  `specialty_id` int(11) NOT NULL,
+  PRIMARY KEY (`attendee_id`),
+  KEY `fk_attendee_specialties` (`specialty_id`),
+  CONSTRAINT `fk_attendee_specialties` FOREIGN KEY (`specialty_id`) REFERENCES `specialties` (`specialty_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `attende`
+--
+
+INSERT INTO `attende` VALUES
+(1,'Marry','Poppins','2009-11-18','marry@email.com','12343567890',1),
+(3,'Vici','Lamm','2021-11-10','vici@email.com','0987654321',1),
+(4,'Gill','Croll','2009-11-01','gill@email.com','0981234',7),
+(5,'Crall','Pram','2015-11-18','crall@email.com','0129384756',7),
+(6,'Jim','Pring','2011-11-07','jim@email.com','999999',8),
+(7,'Paul-Bit','Blummer','2016-11-13','paul@email.com','12222222',6),
+(8,'Dorota','Urbaniak','2015-11-21','dor@email.com','77777',4);
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-04 15:27:25
